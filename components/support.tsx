c164c5cf75fb68e41242af78b586b875207cdfe9
Karan Prakash <karanprakash.ind@gmail.com>
2024-09-09 16:43:45 +0530

    Added: patreon and paypal link

--- components/support.tsx
+++ components/support.tsx
@@ -6,6 +6,8 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import Link from "next/link";
+import { Button } from "./ui/button";
 
 export default function Support({ children }: { children?: React.ReactNode }) {
   return (
@@ -26,6 +28,17 @@ export default function Support({ children }: { children?: React.ReactNode }) {
         ></iframe>
       </CardContent>
       {children}
+      <CardContent>
+        <CardDescription>Support the future development.</CardDescription>
+      </CardContent>
+      <CardFooter className="space-x-4">
+        <Link href="https://patreon.com/B3dHub" target="_blank">
+          <Button variant={"secondary"}>Patreon</Button>
+        </Link>
+        <Link href="https://paypal.me/B3dHub" target="_blank">
+          <Button variant={"secondary"}>Paypal</Button>
+        </Link>
+      </CardFooter>
     </Card>
   );
 }
